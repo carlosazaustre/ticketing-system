f2b9b6896a2e72bc5ca218651fbba5a20768ab66
Carlos Azaustre <cazaustre@gmail.com>
2024-02-27 16:47:09 +0100

    01 Middleware

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ import express from 'express';
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
