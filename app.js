cc29ddf53375c69d7bf4c14768f8dc00e205b0a3
Carlos Azaustre <cazaustre@gmail.com>
2024-02-29 18:32:25 +0100

    04 Mongoose y .Env

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 import express from 'express';
 import morgan from 'morgan';
+import mongoose from 'mongoose';
 import ticketsRoutes from './routes/ticketsRoutes.js';
 
 const app = express();
 const port = process.env.PORT || 3000;
+const DB_URL = process.env.DB_URL
+
+mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('💾 Connected to MongoDB'))
+    .catch(err => console.error('❌ Failed to connect to MongoDB', err));
 
 app.use(morgan('dev'));
 app.use((err, req, res, next) => {
