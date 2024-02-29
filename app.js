410308294fd1e3d8ab66855ff66c462d9a49b812
Carlos Azaustre <cazaustre@gmail.com>
2024-02-29 18:18:00 +0100

    03 Custom Middlewares

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 import express from 'express';
+import morgan from 'morgan';
 import ticketsRoutes from './routes/ticketsRoutes.js';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(morgan('dev'));
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+});
 app.use(express.json());
 
 app.get('/', (req, res) => {
