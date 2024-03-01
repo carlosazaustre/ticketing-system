7be3903b06822f1a009fccc44f321f22ce44f0c2
Carlos Azaustre <cazaustre@gmail.com>
2024-03-01 11:43:30 +0100

    06 Tickets CRUD

--- routes/ticketsRoutes.js
+++ routes/ticketsRoutes.js
@@ -1,21 +1,64 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import Ticket from '../models/Ticket.js';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('All Tickets');
+router.post('/', async (req, res) => {
+    const ticket = new Ticket({
+        user: req.body.userId,
+        title: req.body.title,
+        description: req.body.description,
+        priority: req.body.priority,
+        status: req.body.status
+    });
+
+    try {
+        const newTicket = await ticket.save();
+        res.status(201).json(newTicket);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
 });
-router.post('/', (req, res) => {
-    res.send('New Ticket');
+
+router.get('/', async (req, res) => {
+    try {
+        const tickets = await Ticket.find();
+        res.status(200).send(tickets);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
 });
-router.get('/:id', (req, res) => {
-    res.send('Ticket ' + req.params.id);
+
+router.get('/:id', async (req, res) => {
+    try {
+        const ticket = await Ticket.findById(req.params.id);
+        if (!ticket) return res.status(404).send('Ticket not found.');
+        res.status(200).send(ticket);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
 });
+
 router.put('/:id', (req, res) => {
-    res.send('Update Ticket ' + req.params.id);
+    const updates = req.body;
+    try {
+        const ticket = Ticket.findByIdAndUpdate(req.params.id, updates, { new: true });
+        if (!ticket) return res.status(404).send('Ticket not found.');
+        res.status(200).send(ticket);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
 });
+
 router.delete('/:id', (req, res) => {
-    res.send('Delete Ticket ' + req.params.id);
+    try {
+        const ticket = Ticket.findByIdAndDelete(req.params.id);
+        if (!ticket) return res.status(404).send('Ticket not found.');
+        res.status(204).send(ticket);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
 });
 
 export default router;
