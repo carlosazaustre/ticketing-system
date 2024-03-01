4adadc30465a443aee46623ba2dcc15dc95fdd86
Carlos Azaustre <cazaustre@gmail.com>
2024-03-01 22:46:33 +0100

    fix

--- validations/ticketValidation.js
+++ validations/ticketValidation.js
@@ -1,7 +1,7 @@
 import Joi from 'joi';
 
 const ticketSchema = Joi.object({
-    user: Joi.string().required(),
+    user: Joi.string(),
     title: Joi.string().min(3).required(),
     description: Joi.string().min(5).required(),
     priority: Joi.string().valid('low', 'medium', 'high').required(),
